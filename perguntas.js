64d27c1c39a3b58650e776de56c86cb241469fb0
HELENAOVCAR <HELENA.INAMINE@ESCOLA.PR.GOV.BR>
2025-05-29 10:46:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+    'Biologia',
+    'O que que a fotossíntese?',
+    'Processo de conversão de energia luminosa em química (glicose e oxigênio) por plantas, algas, bactérias.'
+)
+
+criaCartao(
+    'Matemática',
+    'Um carro percorre 240 km em 3 horas. Qual a velocidade média? Se mantiver por +2h, qual a distância total?',
+    'Vm= 240km/3h.= 80km/h
+    Distância adicional: 80 km/h×2 h=160 km.
+    Total: 240 km+160 km=400 km.'
+)
+
+criaCartao(
+    'Robótica',
+    'O que significa a sigla LED?',
+    'Diodo emissor de luz'
+)
